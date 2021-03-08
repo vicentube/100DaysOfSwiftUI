ebdb6bc61f176024e5b01779c3a2bfee9c1e8b9e
vicentube <appeleando@gmail.com>
2021-03-08 00:03:58 -0400

    BetterRest created

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+// BetterRestApp.swift
+// BetterRest
+//
+// Creado el 8/3/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
