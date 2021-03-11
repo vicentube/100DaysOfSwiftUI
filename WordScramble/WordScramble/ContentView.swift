032114a36e2745ba7b94b4dd2b2504e434701611
vicentube <appeleando@gmail.com>
2021-03-11 16:16:41 -0400

    Day 30: Project 5, part 2 - section 2/3

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,32 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
-    let people = ["Finn", "Leia", "Luke", "Rey"]
+  @State private var usedWords = [String]()
+  @State private var rootWord = ""
+  @State private var newWord = ""
 
-    var body: some View {
-      if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-          // we found the file in our bundle!
-        if let fileContents = try? String(contentsOf: fileURL) {
-            // we loaded the file into a string!
+  var body: some View {
+    NavigationView {
+      VStack {
+        TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+          .autocapitalization(.none)
+          .textFieldStyle(RoundedBorderTextFieldStyle())
+          .padding()
+        List(usedWords, id: \.self) {
+          Image(systemName: "\($0.count).circle")
+          Text($0)
         }
       }
-      let input = """
-                  a
-                  b
-                  c
-                  """
-      let letters = input.components(separatedBy: "\n")
-      let letter = letters.randomElement()
-      let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
-      let word = "swift"
-      let checker = UITextChecker()
-      let range = NSRange(location: 0, length: word.utf16.count)
-      let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-      let allGood = misspelledRange.location == NSNotFound
-        List(people, id: \.self) {
-            Text($0)
-        }
+      .navigationBarTitle(rootWord)
+      .onAppear(perform: startGame)
     }
+  }
+  
+  func addNewWord() {
+    // lowercase and trim the word, to make sure we don't add duplicate words with case differences
+    let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+    
+    // exit if the remaining string is empty
+    guard answer.count > 0 else { return }
+    
+    // extra validation to come
+    
+    usedWords.insert(answer, at: 0)
+    newWord = ""
+  }
+  
+  func startGame() {
+    // 1. Find the URL for start.txt in our app bundle
+    if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+      // 2. Load start.txt into a string
+      if let startWords = try? String(contentsOf: startWordsURL) {
+        // 3. Split the string up into an array of strings, splitting on line breaks
+        let allWords = startWords.components(separatedBy: "\n")
+        
+        // 4. Pick one random word, or use "silkworm" as a sensible default
+        rootWord = allWords.randomElement() ?? "silkworm"
+        
+        // If we are here everything has worked, so we can exit
+        return
+      }
+    }
+    
+    // If we are *here* then there was a problem - trigger a crash and report the error
+    fatalError("Could not load start.txt from bundle.")
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
