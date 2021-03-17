0e75e45b63d46d38f67d0626a814a7650e16420c
vicentube <appeleando@gmail.com>
2021-03-17 14:57:06 -0400

    Day 36: Project 7, part 1 - Initial commit

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+// iExpenseApp.swift
+// iExpense
+//
+// Creado el 17/3/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
