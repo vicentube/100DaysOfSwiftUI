bd1a90e83c6034ca70db1ae03d236a3a3f807323
vicentube <appeleando@gmail.com>
2021-03-20 14:34:12 -0400

    Day 39: Project 8, part 1: Initial commit

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+// MoonshotApp.swift
+// Moonshot
+//
+// Creado el 20/3/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
