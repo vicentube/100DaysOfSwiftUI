98c54417eda4dc35917677558a11692364fc0d1e
vicentube <appeleando@gmail.com>
2021-03-23 23:34:07 -0400

    Day 43: Project 9, part 1 - Initial commit

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+// DrawingApp.swift
+// Drawing
+//
+// Creado el 23/3/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
