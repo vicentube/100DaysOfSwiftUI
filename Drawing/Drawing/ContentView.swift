f8483de48865b2a49060b748190c8542cd258fd8
vicentube <appeleando@gmail.com>
2021-03-26 10:07:45 -0400

    Day 45: blendMode(.screen)

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -8,18 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-  var body: some View {
-    ZStack {
-        Image("Example")
-          .colorMultiply(.red)
+    @State private var amount: CGFloat = 0.0
 
-//        Rectangle()
-//            .fill(Color.red)
-//            .blendMode(.multiply)
+    var body: some View {
+        VStack {
+            ZStack {
+                Circle()
+                  .fill(Color(red: 1, green: 0, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -80)
+                    .blendMode(.screen)
+
+                Circle()
+                  .fill(Color(red: 0, green: 1, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: 50, y: -80)
+                    .blendMode(.screen)
+
+                Circle()
+                  .fill(Color(red: 0, green: 0, blue: 1))
+                    .frame(width: 200 * amount)
+                    .blendMode(.screen)
+            }
+            .frame(width: 300, height: 300)
+
+            Slider(value: $amount)
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+        .edgesIgnoringSafeArea(.all)
     }
-    .frame(width: 400, height: 500)
-    .clipped()
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
