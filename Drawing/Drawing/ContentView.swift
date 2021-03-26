bedaf331881ea4aa3579b430d187f9b4bfbaf15a
vicentube <appeleando@gmail.com>
2021-03-26 10:13:24 -0400

    Day 45: animatableData

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,25 +7,39 @@
 
 import SwiftUI
 
+struct Trapezoid: Shape {
+  var insetAmount: CGFloat
+  
+  var animatableData: CGFloat {
+    get { insetAmount }
+    set { self.insetAmount = newValue }
+  }
+  
+  func path(in rect: CGRect) -> Path {
+    var path = Path()
+    
+    path.move(to: CGPoint(x: 0, y: rect.maxY))
+    path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+    path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+    path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+    path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+    
+    return path
+  }
+}
+
 struct ContentView: View {
-    @State private var amount: CGFloat = 0.0
-
-    var body: some View {
-        VStack {
-          Image("Example")
-              .resizable()
-              .scaledToFit()
-              .frame(width: 200, height: 200)
-              .saturation(Double(amount))
-              .blur(radius: (1 - amount) * 20)
-
-            Slider(value: $amount)
-                .padding()
+  @State private var insetAmount: CGFloat = 50
+  
+  var body: some View {
+    Trapezoid(insetAmount: insetAmount)
+      .frame(width: 200, height: 100)
+      .onTapGesture {
+        withAnimation {
+          self.insetAmount = CGFloat.random(in: 10...90)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.black)
-        .edgesIgnoringSafeArea(.all)
-    }
+      }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
