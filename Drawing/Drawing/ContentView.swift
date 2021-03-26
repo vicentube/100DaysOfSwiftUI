e35f1917ee0ee087f9dbbc275429f14ea795f080
vicentube <appeleando@gmail.com>
2021-03-26 10:02:37 -0400

    Day 45: blendMode, colorMultiply

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,45 +7,18 @@
 
 import SwiftUI
 
-struct ColorCyclingCircle: View {
-  var amount = 0.0
-  var steps = 100
-  
+struct ContentView: View {
   var body: some View {
     ZStack {
-      ForEach(0..<steps) { value in
-        Circle()
-          .inset(by: CGFloat(value))
-          .strokeBorder(LinearGradient(gradient: Gradient(colors: [
-              self.color(for: value, brightness: 1),
-              self.color(for: value, brightness: 0.5)
-          ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
-      }
-    }
-    .drawingGroup()
-  }
-  
-  func color(for value: Int, brightness: Double) -> Color {
-    var targetHue = Double(value) / Double(self.steps) + self.amount
-    
-    if targetHue > 1 {
-      targetHue -= 1
-    }
-    
-    return Color(hue: targetHue, saturation: 1, brightness: brightness)
-  }
-}
+        Image("Example")
+          .colorMultiply(.red)
 
-struct ContentView: View {
-  @State private var colorCycle = 0.0
-  
-  var body: some View {
-    VStack {
-      ColorCyclingCircle(amount: self.colorCycle)
-        .frame(width: 300, height: 300)
-      
-      Slider(value: $colorCycle)
+//        Rectangle()
+//            .fill(Color.red)
+//            .blendMode(.multiply)
     }
+    .frame(width: 400, height: 500)
+    .clipped()
   }
 }
 
