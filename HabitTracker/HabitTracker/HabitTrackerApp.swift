504fa2286fcf713c2596218a0254362ab454fb59
vicentube <appeleando@gmail.com>
2021-04-01 10:56:00 -0400

    Day 47: Add Habit struct

---
+++ HabitTracker/HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+// HabitTrackerApp.swift
+// HabitTracker
+//
+// Creado el 1/4/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ HabitTracker/HabitTracker/Model/Habit.swift
@@ -0,0 +1,14 @@
+// Habit.swift
+// HabitTracker
+//
+// Creado el 1/4/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import Foundation
+
+struct Habit: Identifiable {
+  let id = UUID()
+  var title: String
+  var description: String
+}
