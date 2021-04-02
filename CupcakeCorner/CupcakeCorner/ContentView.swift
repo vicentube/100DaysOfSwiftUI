79c66f590aa8135fd7e948b49517c4afcc6f2577
vicentube <appeleando@gmail.com>
2021-04-02 15:31:08 -0400

    Day 49: Initial commit

---
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,21 @@
+// ContentView.swift
+// CupcakeCorner
+//
+// Creado el 2/4/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+// CupcakeCornerApp.swift
+// CupcakeCorner
+//
+// Creado el 2/4/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
