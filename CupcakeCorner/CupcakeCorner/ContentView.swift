6839b396702e02bc62e00d661364c4abaa808268
vicentube <appeleando@gmail.com>
2021-04-02 15:50:18 -0400

    Day 49: Add Codable conformance for Published properties

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -19,3 +19,21 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+class User: ObservableObject, Codable {
+  @Published var name = "Paul Hudson"
+  
+  enum CodingKeys: CodingKey {
+    case name
+  }
+  
+  required init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    name = try container.decode(String.self, forKey: .name)
+  }
+  
+  func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    try container.encode(name, forKey: .name)
+  }
+}
