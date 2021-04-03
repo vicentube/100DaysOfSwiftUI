b1bd1a8227b52dd28cd951e6b4bfd50d3d452aec
vicentube <appeleando@gmail.com>
2021-04-02 22:30:27 -0400

    Day 49: Validating and disabling forms

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -8,73 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var results = [Result]()
+  @State private var username = ""
+  @State private var email = ""
   
-  var body: some View {
-    List(results, id: \.trackId) { item in
-      VStack(alignment: .leading) {
-        Text(item.trackName)
-          .font(.headline)
-        Text(item.collectionName)
-      }
-    }
-    .onAppear(perform: loadData)
+  var disableForm: Bool {
+    username.count < 5 || email.count < 5
   }
   
-  func loadData() {
-    guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-      print("Invalid URL")
-      return
-    }
-    let request = URLRequest(url: url)
-    URLSession.shared.dataTask(with: request) { data, response, error in
-      if let data = data {
-        if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-          // we have good data – go back to the main thread
-          DispatchQueue.main.async {
-            // update our UI
-            self.results = decodedResponse.results
-          }
-          // everything is good, so we can exit
-          return
+  var body: some View {
+    Form {
+      Section {
+        TextField("Username", text: $username)
+        TextField("Email", text: $email)
+      }
+      
+      Section {
+        Button("Create account") {
+          print("Creating account…")
         }
       }
-      // if we're still here it means there was a problem
-      print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
-    }.resume()
+      .disabled(disableForm)
+    }
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
-  }
-}
-
-struct Response: Codable {
-  var results: [Result]
-}
-
-struct Result: Codable {
-  var trackId: Int
-  var trackName: String
-  var collectionName: String
-}
-
-class User: ObservableObject, Codable {
-  @Published var name = "Paul Hudson"
-  
-  enum CodingKeys: CodingKey {
-    case name
-  }
-  
-  required init(from decoder: Decoder) throws {
-    let container = try decoder.container(keyedBy: CodingKeys.self)
-    name = try container.decode(String.self, forKey: .name)
-  }
-  
-  func encode(to encoder: Encoder) throws {
-    var container = encoder.container(keyedBy: CodingKeys.self)
-    try container.encode(name, forKey: .name)
   }
 }
