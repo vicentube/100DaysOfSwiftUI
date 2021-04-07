4ea8b6698b07030e76644f3f2873e04290a0ca0f
Vicente Úbeda <appeleando@gmail.com>
2021-04-07 13:40:02 -0400

    Day 53: How to combine Core Data and SwiftUI

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -9,110 +9,74 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.horizontalSizeClass) var sizeClass
-
-    var body: some View {
-      if sizeClass == .compact {
-          return AnyView(VStack {
-              Text("Active size class:")
-              Text("COMPACT")
-          }
-          .font(.largeTitle))
-      } else {
-          return AnyView(HStack {
-              Text("Active size class:")
-              Text("REGULAR")
-          }
-          .font(.largeTitle))
+  @Environment(\.managedObjectContext) private var moc
+  
+  @FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
+  
+  var body: some View {
+    VStack {
+      List {
+        ForEach(students, id: \.id) { student in
+          Text(student.name ?? "Unknown")
+        }
+      }
+      Button("Add") {
+        let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+        let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+        
+        let chosenFirstName = firstNames.randomElement()!
+        let chosenLastName = lastNames.randomElement()!
+        
+        let student = Student(context: self.moc)
+        student.id = UUID()
+        student.name = "\(chosenFirstName) \(chosenLastName)"
+        
+        try? moc.save()
       }
     }
+  }
+  
+  private func addItem() {
+    withAnimation {
+      let newItem = Student(context: moc)
+      newItem.name = "Hello world!"
+      
+      do {
+        try moc.save()
+      } catch {
+        // Replace this implementation with code to handle the error appropriately.
+        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        let nsError = error as NSError
+        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+      }
+    }
+  }
+  
+  private func deleteItems(offsets: IndexSet) {
+    withAnimation {
+      offsets.map { students[$0] }.forEach(moc.delete)
+      
+      do {
+        try moc.save()
+      } catch {
+        // Replace this implementation with code to handle the error appropriately.
+        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        let nsError = error as NSError
+        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+      }
+    }
+  }
 }
-//
-//    @Environment(\.managedObjectContext) private var viewContext
-//
-//    @FetchRequest(
-//        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-//        animation: .default)
-//    private var items: FetchedResults<Item>
-//
-//    var body: some View {
-//        List {
-//            ForEach(items) { item in
-//                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-//            }
-//            .onDelete(perform: deleteItems)
-//        }
-//        .toolbar {
-//            #if os(iOS)
-//            EditButton()
-//            #endif
-//
-//            Button(action: addItem) {
-//                Label("Add Item", systemImage: "plus")
-//            }
-//        }
-//    }
 
-//    private func addItem() {
-//        withAnimation {
-//            let newItem = Item(context: viewContext)
-//            newItem.timestamp = Date()
-//
-//            do {
-//                try viewContext.save()
-//            } catch {
-//                // Replace this implementation with code to handle the error appropriately.
-//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//                let nsError = error as NSError
-//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-//            }
-//        }
-//    }
-//
-//    private func deleteItems(offsets: IndexSet) {
-//        withAnimation {
-//            offsets.map { items[$0] }.forEach(viewContext.delete)
-//
-//            do {
-//                try viewContext.save()
-//            } catch {
-//                // Replace this implementation with code to handle the error appropriately.
-//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//                let nsError = error as NSError
-//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-//            }
-//        }
-//    }
-//}
-//
-//private let itemFormatter: DateFormatter = {
-//    let formatter = DateFormatter()
-//    formatter.dateStyle = .short
-//    formatter.timeStyle = .medium
-//    return formatter
-//}()
+private let itemFormatter: DateFormatter = {
+  let formatter = DateFormatter()
+  formatter.dateStyle = .short
+  formatter.timeStyle = .medium
+  return formatter
+}()
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-    }
-}
-
-struct PushButton: View {
-    let title: String
-    @Binding var isOn: Bool
-
-    var onColors = [Color.red, Color.yellow]
-    var offColors = [Color(white: 0.6), Color(white: 0.4)]
-
-    var body: some View {
-        Button(title) {
-            self.isOn.toggle()
-        }
-        .padding()
-        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
-        .foregroundColor(.white)
-        .clipShape(Capsule())
-        .shadow(radius: isOn ? 0 : 5)
-    }
+  static var previews: some View {
+    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+  }
 }
