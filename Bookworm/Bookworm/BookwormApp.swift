761e5f32fbdc2c25f20099fc9c216d1c580fbbfa
Vicente Úbeda <appeleando@gmail.com>
2021-04-07 10:37:25 -0400

    Day 53: Initial commit

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,20 @@
+// BookwormApp.swift
+// Bookworm
+//
+// Creado el 7/4/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
