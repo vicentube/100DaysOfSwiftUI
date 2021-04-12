675182442758bdfde5ca20ce9d661d13d971d488
Vicente Úbeda <appeleando@gmail.com>
2021-04-12 17:23:17 -0400

    Day 57: Project 12 - Initial commit

---
+++ CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,20 @@
+// CoreDataProjectApp.swift
+// CoreDataProject
+//
+// Creado el 12/4/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
