6d17266571c4b7fd74d8f9af897522af29aa2271
Vicente Úbeda <appeleando@gmail.com>
2021-04-13 15:23:54 -0400

    Day 58: Filter FetchRequest using NSPredicate

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -9,72 +9,54 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
-    var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-            }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
-            }
-        }
+  @Environment(\.managedObjectContext) var moc
+  @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: nil) var ships: FetchedResults<Ship>
+  // NSPredicate(format: "universe == 'Star Wars'")
+  // NSPredicate(format: "universe == %@", "Star Wars")
+  // NSPredicate(format: "name < %@", "F")
+  // NSPredicate(format: "universe IN %@", ["Aliens", "Firefly", "Star Trek"])
+  // NSPredicate(format: "name BEGINSWITH %@", "E"))
+  // NSPredicate(format: "name BEGINSWITH[c] %@", "e"))
+  // NSPredicate(format: "NOT name BEGINSWITH[c] %@", "e"))
+  
+  var body: some View {
+    VStack {
+      List(ships, id: \.self) { ship in
+        Text(ship.name ?? "Unknown name")
+      }
+      
+      Button("Add Examples") {
+        let ship1 = Ship(context: self.moc)
+        ship1.name = "Enterprise"
+        ship1.universe = "Star Trek"
+        
+        let ship2 = Ship(context: self.moc)
+        ship2.name = "Defiant"
+        ship2.universe = "Star Trek"
+        
+        let ship3 = Ship(context: self.moc)
+        ship3.name = "Millennium Falcon"
+        ship3.universe = "Star Wars"
+        
+        let ship4 = Ship(context: self.moc)
+        ship4.name = "Executor"
+        ship4.universe = "Star Wars"
+        
+        try? self.moc.save()
+      }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+  }
 }
 
 private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
+  let formatter = DateFormatter()
+  formatter.dateStyle = .short
+  formatter.timeStyle = .medium
+  return formatter
 }()
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-    }
+  static var previews: some View {
+    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+  }
 }
