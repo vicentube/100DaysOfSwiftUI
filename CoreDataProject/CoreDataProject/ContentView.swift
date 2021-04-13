703f48b583f2d571370f5c0aca626c9c7a1285f1
Vicente Úbeda <appeleando@gmail.com>
2021-04-13 15:52:23 -0400

    Day 58: Dynamically filter FetchRequest with SwiftUI

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -10,39 +10,36 @@ import CoreData
 
 struct ContentView: View {
   @Environment(\.managedObjectContext) var moc
-  @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: nil) var ships: FetchedResults<Ship>
-  // NSPredicate(format: "universe == 'Star Wars'")
-  // NSPredicate(format: "universe == %@", "Star Wars")
-  // NSPredicate(format: "name < %@", "F")
-  // NSPredicate(format: "universe IN %@", ["Aliens", "Firefly", "Star Trek"])
-  // NSPredicate(format: "name BEGINSWITH %@", "E"))
-  // NSPredicate(format: "name BEGINSWITH[c] %@", "e"))
-  // NSPredicate(format: "NOT name BEGINSWITH[c] %@", "e"))
+  @State private var lastNameFilter = "A"
   
   var body: some View {
     VStack {
-      List(ships, id: \.self) { ship in
-        Text(ship.name ?? "Unknown name")
+      FilteredList(filterKey: "lastName", filterValue: lastNameFilter) { (singer: Singer) in
+          Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
       }
       
       Button("Add Examples") {
-        let ship1 = Ship(context: self.moc)
-        ship1.name = "Enterprise"
-        ship1.universe = "Star Trek"
+        let taylor = Singer(context: self.moc)
+        taylor.firstName = "Taylor"
+        taylor.lastName = "Swift"
         
-        let ship2 = Ship(context: self.moc)
-        ship2.name = "Defiant"
-        ship2.universe = "Star Trek"
+        let ed = Singer(context: self.moc)
+        ed.firstName = "Ed"
+        ed.lastName = "Sheeran"
         
-        let ship3 = Ship(context: self.moc)
-        ship3.name = "Millennium Falcon"
-        ship3.universe = "Star Wars"
-        
-        let ship4 = Ship(context: self.moc)
-        ship4.name = "Executor"
-        ship4.universe = "Star Wars"
+        let adele = Singer(context: self.moc)
+        adele.firstName = "Adele"
+        adele.lastName = "Adkins"
         
         try? self.moc.save()
+      }
+      
+      Button("Show A") {
+        self.lastNameFilter = "A"
+      }
+      
+      Button("Show S") {
+        self.lastNameFilter = "S"
       }
     }
   }
---
+++ CoreDataProject/CoreDataProject/FilteredList.swift
@@ -0,0 +1,29 @@
+// FilteredList.swift
+// CoreDataProject
+//
+// Creado el 13/4/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+import CoreData
+
+struct FilteredList<T: NSManagedObject, Content: View>: View {
+  var fetchRequest: FetchRequest<T>
+  var singers: FetchedResults<T> { fetchRequest.wrappedValue }
+  
+  // this is our content closure; we'll call this once for each item in the list
+  let content: (T) -> Content
+  
+  var body: some View {
+    List(fetchRequest.wrappedValue, id: \.self) { singer in
+      self.content(singer)
+    }
+  }
+  
+  init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
+    fetchRequest = FetchRequest<T>(entity: T.entity(), sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+    self.content = content
+  }
+}
+
