9a8f83194e11a1494f2c919e436e7bfaf63770d8
Vicente Úbeda <appeleando@gmail.com>
2021-04-18 08:01:17 -0400

    Day 62: Instafilter - Initial commit

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+// InstafilterApp.swift
+// Instafilter
+//
+// Creado el 18/4/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
