caa7aed34e13f8e38cbe1db84dd69d1dc4f05a1a
Vicente Úbeda <appeleando@gmail.com>
2021-04-20 10:06:51 -0400

    Day 64: Using coordinators to manage SwiftUI view controllers

--- Instafilter/Instafilter/TestView2.swift
+++ Instafilter/Instafilter/TestView2.swift
@@ -12,6 +12,7 @@ import CoreImage.CIFilterBuiltins
 struct TestView2: View {
   @State private var image: Image?
   @State private var showingImagePicker = false
+  @State private var inputImage: UIImage?
   
   var body: some View {
     VStack {
@@ -22,48 +23,53 @@ struct TestView2: View {
       Button("Select Image") {
         self.showingImagePicker = true
       }
-      .sheet(isPresented: $showingImagePicker) {
-        ImagePicker()
+      .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+        ImagePicker(image: $inputImage)
       }
     }
   }
   
   func loadImage() {
-    guard let inputImage = UIImage(named: "Example") else { return }
-    let beginImage = CIImage(image: inputImage)
-    
-    let context = CIContext()
-    
-    //    let currentFilter = CIFilter.sepiaTone()
-    //    currentFilter.inputImage = beginImage
-    //    currentFilter.intensity = 1
-    
-    //    let currentFilter = CIFilter.pixellate()
-    //    currentFilter.inputImage = beginImage
-    //    currentFilter.scale = 100
-    
-    //    let currentFilter = CIFilter.crystallize()
-    //        //currentFilter.inputImage = beginImage
-    //        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
-    //    currentFilter.radius = 200
-    
-    guard let currentFilter = CIFilter(name: "CITwirlDistortion") else { return }
-    currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
-    currentFilter.setValue(2000, forKey: kCIInputRadiusKey)
-    currentFilter.setValue(CIVector(x: inputImage.size.width / 2, y: inputImage.size.height / 2), forKey: kCIInputCenterKey)
-    
-    // get a CIImage from our filter or exit if that fails
-    guard let outputImage = currentFilter.outputImage else { return }
-    
-    // attempt to get a CGImage from our CIImage
-    if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-      // convert that to a UIImage
-      let uiImage = UIImage(cgImage: cgimg)
-      
-      // and convert that to a SwiftUI image
-      image = Image(uiImage: uiImage)
-    }
+    guard let inputImage = inputImage else { return }
+    image = Image(uiImage: inputImage)
   }
+  
+//  func loadImage() {
+//    guard let inputImage = UIImage(named: "Example") else { return }
+//    let beginImage = CIImage(image: inputImage)
+//
+//    let context = CIContext()
+//
+//    //    let currentFilter = CIFilter.sepiaTone()
+//    //    currentFilter.inputImage = beginImage
+//    //    currentFilter.intensity = 1
+//
+//    //    let currentFilter = CIFilter.pixellate()
+//    //    currentFilter.inputImage = beginImage
+//    //    currentFilter.scale = 100
+//
+//    //    let currentFilter = CIFilter.crystallize()
+//    //        //currentFilter.inputImage = beginImage
+//    //        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+//    //    currentFilter.radius = 200
+//
+//    guard let currentFilter = CIFilter(name: "CITwirlDistortion") else { return }
+//    currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+//    currentFilter.setValue(2000, forKey: kCIInputRadiusKey)
+//    currentFilter.setValue(CIVector(x: inputImage.size.width / 2, y: inputImage.size.height / 2), forKey: kCIInputCenterKey)
+//
+//    // get a CIImage from our filter or exit if that fails
+//    guard let outputImage = currentFilter.outputImage else { return }
+//
+//    // attempt to get a CGImage from our CIImage
+//    if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+//      // convert that to a UIImage
+//      let uiImage = UIImage(cgImage: cgimg)
+//
+//      // and convert that to a SwiftUI image
+//      image = Image(uiImage: uiImage)
+//    }
+//  }
 }
 
 struct TestView2_Previews: PreviewProvider {
