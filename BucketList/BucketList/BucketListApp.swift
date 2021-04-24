d769a24f9fb3b5e3f62818fff8d558a0b337dbff
Vicente Úbeda <appeleando@gmail.com>
2021-04-24 15:13:41 -0400

    Day 68: Initial commit

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+// BucketListApp.swift
+// BucketList
+//
+// Creado el 24/4/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
