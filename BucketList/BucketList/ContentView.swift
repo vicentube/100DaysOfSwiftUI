91389a5ecab7261dd75ab2799ba4150049775f10
Vicente Úbeda <appeleando@gmail.com>
2021-04-25 18:27:20 -0400

    Day 69: Using Touch ID and Face ID with SwiftUI

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -6,11 +6,44 @@
 // Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
 
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
+  @State private var isUnlocked = false
+  
   var body: some View {
-    MapView()
-      .edgesIgnoringSafeArea(.all)
+    VStack {
+      if isUnlocked {
+        Text("Unlocked")
+      } else {
+        Text("Locked")
+      }
+    }
+    .onAppear(perform: authenticate)
+  }
+  
+  func authenticate() {
+    let context = LAContext()
+    var error: NSError?
+    
+    // check whether biometric authentication is possible
+    if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+      // it's possible, so go ahead and use it
+      let reason = "We need to unlock your data."
+      
+      context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+        // authentication has now completed
+        DispatchQueue.main.async {
+          if success {
+            isUnlocked = true
+          } else {
+            // there was a problem
+          }
+        }
+      }
+    } else {
+      // no biometrics
+    }
   }
 }
 
