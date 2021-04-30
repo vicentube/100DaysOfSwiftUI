ee42a3fdc3a14a64176ed91ae168300e3d5d5a0e
Vicente Úbeda <appeleando@gmail.com>
2021-04-29 20:09:51 -0400

    Day 73: Challenge 2/3

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -10,48 +10,12 @@ import MapKit
 import LocalAuthentication
 
 struct ContentView: View {
-  @State private var centerCoordinate = CLLocationCoordinate2D()
-  @State private var locations = [CodableMKPointAnnotation]()
-  @State private var selectedPlace: MKPointAnnotation?
-  @State private var showingPlaceDetails = false
-  @State private var showingEditScreen = false
   @State private var isUnlocked = false
   
   var body: some View {
     ZStack {
       if isUnlocked {
-        MapView(
-          centerCoordinate: $centerCoordinate,
-          selectedPlace: $selectedPlace,
-          showingPlaceDetails: $showingPlaceDetails,
-          annotations: locations)
-          .edgesIgnoringSafeArea(.all)
-        Circle()
-          .fill(Color.blue)
-          .opacity(0.3)
-          .frame(width: 32, height: 32)
-        VStack {
-          Spacer()
-          HStack {
-            Spacer()
-            Button(action: {
-              let newLocation = CodableMKPointAnnotation()
-              newLocation.title = "Example location"
-              newLocation.coordinate = centerCoordinate
-              locations.append(newLocation)
-              selectedPlace = newLocation
-              showingEditScreen = true
-            }) {
-              Image(systemName: "plus")
-                .padding()
-                .background(Color.black.opacity(0.75))
-                .foregroundColor(.white)
-                .font(.title)
-                .clipShape(Circle())
-                .padding(.trailing)
-            }
-          }
-        }
+        UnlockedView()
       } else {
         Button("Unlock Places") {
           self.authenticate()
@@ -61,47 +25,6 @@ struct ContentView: View {
         .foregroundColor(.white)
         .clipShape(Capsule())
       }
-    }
-    .alert(isPresented: $showingPlaceDetails) {
-      Alert(
-        title: Text(selectedPlace?.title ?? "Unknown"),
-        message: Text(selectedPlace?.subtitle ?? "Missing place information"),
-        primaryButton: .default(Text("OK")),
-        secondaryButton: .default(Text("Edit")) {
-          showingEditScreen = true
-        })
-    }
-    .sheet(isPresented: $showingEditScreen, onDismiss: saveData) {
-      if let selectedPlace = selectedPlace {
-        EditView(placemark: selectedPlace)
-      }
-    }
-    .onAppear(perform: loadData)
-  }
-  
-  func getDocumentsDirectory() -> URL {
-    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-    return paths[0]
-  }
-  
-  func loadData() {
-    let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
-    
-    do {
-      let data = try Data(contentsOf: filename)
-      locations = try JSONDecoder().decode([CodableMKPointAnnotation].self, from: data)
-    } catch {
-      print("Unable to load saved data.")
-    }
-  }
-  
-  func saveData() {
-    do {
-      let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
-      let data = try JSONEncoder().encode(locations)
-      try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
-    } catch {
-      print("Unable to save data.")
     }
   }
   
