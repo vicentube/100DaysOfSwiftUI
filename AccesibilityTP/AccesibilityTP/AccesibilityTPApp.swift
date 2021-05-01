ed9d47b8647c36cfe5b6e317e7d0633c1f26a347
Vicente Úbeda <appeleando@gmail.com>
2021-04-30 20:40:33 -0400

    Day 74: Initial commit

---
+++ AccesibilityTP/AccesibilityTP/AccesibilityTPApp.swift
@@ -0,0 +1,17 @@
+// AccesibilityTPApp.swift
+// AccesibilityTP
+//
+// Creado el 30/4/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+@main
+struct AccesibilityTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
