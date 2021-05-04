309eac04feee11167efa3c3798d7b892b7373cb9
Vicente Úbeda <appeleando@gmail.com>
2021-05-03 22:59:59 -0400

    Day 77: Initial commit

---
+++ PeopleNames/PeopleNames/PeopleNamesApp.swift
@@ -0,0 +1,20 @@
+// PeopleNamesApp.swift
+// PeopleNames
+//
+// Creado el 3/5/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+@main
+struct PeopleNamesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
