b52b628fe27ed79d4b2fb993cf6d1ea3f7f8b8b2
Vicente Úbeda <appeleando@gmail.com>
2021-05-03 23:19:57 -0400

    ImagePicker

--- PeopleNames/PeopleNames/PeopleNamesApp.swift
+++ PeopleNames/PeopleNames/PeopleNamesApp.swift
@@ -13,8 +13,8 @@ struct PeopleNamesApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+//            ContentView()
+//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
