a9376ceaae9485defd9be6f02a88465beb8193ed
Vicente Úbeda <appeleando@gmail.com>
2021-05-08 15:19:39 -0400

    Day 79: Initial commit

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+// HotProspectsApp.swift
+// HotProspects
+//
+// Creado el 8/5/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
