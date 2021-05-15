c020d888fd7cde96d52a90240d39fbdf0c6f4263
Vicente Úbeda <appeleando@gmail.com>
2021-05-15 16:18:09 -0400

    Day 86: Flashzilla initial commit

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+// FlashzillaApp.swift
+// Flashzilla
+//
+// Creado el 15/5/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
