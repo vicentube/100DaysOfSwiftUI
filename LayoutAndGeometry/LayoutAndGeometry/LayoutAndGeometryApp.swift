3967b53baf1700f528c29811c303a03724a25d1d
Vicente Úbeda <appeleando@gmail.com>
2021-05-21 07:52:20 -0400

    Day 92: Initial commit

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+// LayoutAndGeometryApp.swift
+// LayoutAndGeometry
+//
+// Creado el 21/5/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
