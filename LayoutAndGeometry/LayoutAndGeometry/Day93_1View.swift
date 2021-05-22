0f165b12e17cbd8f7ff001d86b470f5b7e133716
Vicente Úbeda <appeleando@gmail.com>
2021-05-22 16:28:51 -0400

    Day 93: Absolute positioning for SwiftUI views

---
+++ LayoutAndGeometry/LayoutAndGeometry/Day93_1View.swift
@@ -0,0 +1,22 @@
+// Day93_1View.swift
+// LayoutAndGeometry
+//
+// Creado el 22/5/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+struct Day93_1View: View {
+  var body: some View {
+    Text("Hello, World!")
+      .background(Color.red)
+      .offset(x: 100, y: 100)
+  }
+}
+
+struct Day93_1View_Previews: PreviewProvider {
+    static var previews: some View {
+        Day93_1View()
+    }
+}
