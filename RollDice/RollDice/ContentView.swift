07bf45a83d1887d903ec2974d4432e3c22ccf2e5
Vicente Úbeda <appeleando@gmail.com>
2021-05-24 22:17:11 -0400

    Day 95: Initial commit

---
+++ RollDice/RollDice/ContentView.swift
@@ -0,0 +1,21 @@
+// ContentView.swift
+// RollDice
+//
+// Creado el 24/5/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello world")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RollDice/RollDice/RollDiceApp.swift
@@ -0,0 +1,20 @@
+// RollDiceApp.swift
+// RollDice
+//
+// Creado el 24/5/21 por Vicente Úbeda (@vicentube)
+// https://appeleando.com
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+@main
+struct RollDiceApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                //.environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
